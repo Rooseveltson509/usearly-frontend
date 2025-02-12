59dc3bdbefb4b93f36ee501654adec42afbc6411
Rooseveltson509 <RooseveltsonC@gmail.com>
2025-02-12 10:43:11 +0100

    Fix Vercel routing issue (#3)
    Co-authored-by: usearly zenity <git@usearly.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@src': path.resolve(__dirname, './src'),
+      "@src": path.resolve(__dirname, "./src"),
     },
   },
   optimizeDeps: {
     include: [
-      'swiper/react',
-      'swiper/modules',
-      'swiper/css',
-      'swiper/css/navigation',
-      'swiper/css/pagination',
-      'swiper/css/autoplay',
+      "swiper/react",
+      "swiper/modules",
+      "swiper/css",
+      "swiper/css/navigation",
+      "swiper/css/pagination",
+      "swiper/css/autoplay",
     ],
+  },
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 3000,
   },
 });
